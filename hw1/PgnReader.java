812c42a1ac5f0aeb687901de81dbc4473cb8035c
Nick <nsenador1@gmail.com>
2017-09-14 12:16:17 -0400

    Implemeted tagValue() in PgnReader.java and started finalPosition(). Also added .pgn games.

--- hw1/PgnReader.java
+++ hw1/PgnReader.java
@@ -1,4 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PgnReader {
+
+    private static String[][] board = {
+        {"r", "n", "b", "q", "k", "b", "n", "r"},
+        {"p", "p", "p", "p", "p", "p", "p", "p"},
+        {"", "", "", "", "", "", "", ""},
+        {"", "", "", "", "", "", "", ""},
+        {"", "", "", "", "", "", "", ""},
+        {"", "", "", "", "", "", "", ""},
+        {"P", "P", "P", "P", "P", "P", "P", "P"},
+        {"R", "N", "B", "Q", "K", "B", "N", "R"},
+    };
 
     public static void main(String[] args) {
         String game = fileContent(args[0]);
@@ -13,11 +30,44 @@ public class PgnReader {
         System.out.println(finalPosition(game));
     }
 
-    public static String tagValue(String tagName, String gameInPgn) {
+    public static String fileContent(String path) {
+        Path file = Paths.get(path);
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = Files.newBufferedReader(file)) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+            System.exit(1);
+        }
+        return sb.toString();
+    }
 
+    public static String tagValue(String tagName, String gameInPgn) {
+        int tagIndex = gameInPgn.indexOf(tagName);
+        if (tagIndex > 0) {
+            StringBuilder sb = new StringBuilder();
+            int i = tagIndex + tagName.length();
+            while (gameInPgn.charAt(i) != '"') {
+                i++;
+            }
+            i++;
+            // i is at index of the first character of tag value
+            while (gameInPgn.charAt(i) != '"') {
+                sb.append(gameInPgn.charAt(i));
+                i++;
+            }
+            return sb.toString();
+        } else {
+            return "NOT GIVEN";
+        }
     }
 
     public static String finalPosition(String gameInPgn) {
-
+        String[] splitGame = gameInPgn.split("\\n1\\. ");
+        String[] moves = splitGame[1].split("\\s");
+        return "";
     }
 }
